39695b29e8598fa5a29ddd12e81c4ffc960ce56b
santiagoae <santiago_avendano82162@elpoli.edu.co>
2024-03-29 19:21:29 -0500

    add  adapter for success deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
-
 import react from "@astrojs/react";
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   site: 'https://santiagoae.github.io'
 });
