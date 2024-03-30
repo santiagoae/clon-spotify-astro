d3a08028ba84311a8ff12321bc569b4d6e88dd41
santiagoae <santiago_avendano82162@elpoli.edu.co>
2024-03-29 20:48:11 -0500

    rollback base project functional

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
-import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), react()],
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  site: 'https://santiagoae.github.io',
-  base: '/clon-spotify-astro'
 });
